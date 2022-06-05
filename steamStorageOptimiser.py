f8390ed66bf943815af5a8d2ab75c7422485cd44
Jake Martin <jm917@ic.ac.uk>
2022-06-05 20:02:33 +0100

    Style

--- steamStorageOptimiser.py
+++ steamStorageOptimiser.py
@@ -17,33 +17,41 @@ def load_config():
         print(f"{Fore.GREEN}Loaded config file.{Style.RESET_ALL} To change, delete the config file to run setup again, or edit it directly.")
     except:
         print(f"{Fore.YELLOW}No config file found.{Style.RESET_ALL}")
-        key = input("Enter your API key (create one here: https://steamcommunity.com/dev/apikey (domain is irrelevant)): ")
-        steamid = input("Enter your 64-bit SteamID (eg. use this tool https://www.steamidfinder.com/): ")
-        config = {"key" : key, "steamid" : steamid, "install_dir" : "C:\Program Files (x86)\Steam\steamapps"}
+        key = input(
+            "Enter your API key (create one here: https://steamcommunity.com/dev/apikey (domain is irrelevant)): ")
+        steamid = input(
+            "Enter your 64-bit SteamID (eg. use this tool https://www.steamidfinder.com/): ")
+        config = {"key": key, "steamid": steamid,
+                  "install_dir": "C:\Program Files (x86)\Steam\steamapps"}
         with open("config.json", 'w') as f:
             json.dump(config, f)
-        input(f"{Fore.GREEN}Saved new config file.{Style.RESET_ALL} Press any key to continue.")
+        input(
+            f"{Fore.GREEN}Saved new config file.{Style.RESET_ALL} Press any key to continue.")
     return config
+
 
 def get_api_response(config):
     url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1"
-    payload = {'key': config['key'], 'steamid': config['steamid'], 'include_appinfo' : True}
+    payload = {'key': config['key'],
+               'steamid': config['steamid'], 'include_appinfo': True}
 
-    api_response = requests.get(url, params = payload)
+    api_response = requests.get(url, params=payload)
     try:
         api_response = api_response.json()["response"]["games"]
     except:
         print(f"{Fore.RED}API response invalid.{Style.RESET_ALL} Expected data, recieved:\n{api_response.text}. \n{Fore.YELLOW}Check your config?{Style.RESET_ALL}")
         os.system("pause")
         sys.exit(1)
-    return {game["appid"] : game for game in api_response}
+    return {game["appid"]: game for game in api_response}
+
 
 def get_library_paths(config):
     try:
         with open(f"{config['install_dir']}\\libraryfolders.vdf") as f:
             libraries = vdf.parse(f)["libraryfolders"]
     except:
-        print(f"{Fore.RED}Problem with libraryfolders file.{Style.RESET_ALL} If Steam not installed at {Fore.YELLOW}{config['install_dir']}{Style.RESET_ALL}, edit your config file and try again")
+        print(
+            f"{Fore.RED}Problem with libraryfolders file.{Style.RESET_ALL} If Steam not installed at {Fore.YELLOW}{config['install_dir']}{Style.RESET_ALL}, edit your config file and try again")
         os.system("pause")
         sys.exit(1)
 
@@ -52,13 +60,16 @@ def get_library_paths(config):
         library_paths.append(library["path"] + "\\steamapps")
     return library_paths
 
+
 def match_games(config, api_dict):
     games = []
     unmatched_games = []
 
     for library_path in get_library_paths(config):
-        manifests = [ f.path for f in os.scandir(library_path) if not f.is_dir() and f.name[0] == 'a' ]
-        print(f"{Fore.GREEN}Found {len(manifests)} in library {library_path}{Style.RESET_ALL}")
+        manifests = [f.path for f in os.scandir(
+            library_path) if not f.is_dir() and f.name[0] == 'a']
+        print(
+            f"{Fore.GREEN}Found {len(manifests)} in library {library_path}{Style.RESET_ALL}")
         for manifest in manifests:
             try:
                 with open(manifest) as f:
@@ -68,25 +79,27 @@ def match_games(config, api_dict):
                     try:
                         api_info = api_dict[appid]
                     except KeyError as e:
-                        print(f"{Style.DIM}Failed to match {appid}: {game['name']}.{Style.RESET_ALL}")
+                        print(
+                            f"{Style.DIM}Failed to match {appid}: {game['name']}.{Style.RESET_ALL}")
                     else:
                         playtime = api_info["playtime_forever"]
                         size = int(game["SizeOnDisk"])
                         if size != 0:
-                            games.append({"name" : game["name"], 
-                                            "size" : size,
-                                            "sizeGB" : prettySize(size),
-                                            "playtime" : playtime,
-                                            "playtimeH" : '{:02d}:{:02d}'.format(*divmod(playtime, 60)),
-                                            "timePerByte" : playtime/size,
-                                            "hoursPerGB": (playtime/60)/(size/1073741824)})
+                            games.append({"name": game["name"],
+                                          "size": size,
+                                          "sizeGB": prettySize(size),
+                                          "playtime": playtime,
+                                          "playtimeH": '{:02d}:{:02d}'.format(*divmod(playtime, 60)),
+                                          "timePerByte": playtime/size,
+                                          "hoursPerGB": (playtime/60)/(size/1073741824)})
                         else:
-                            unmatched_games.append({"name" : game["name"],
-                                                    "playtime" : playtime,
+                            unmatched_games.append({"name": game["name"],
+                                                    "playtime": playtime,
                                                     "playtimeH": '{:02d}:{:02d}'.format(*divmod(playtime, 60))})
             except SyntaxError:
                 pass
     return (games, unmatched_games)
+
 
 def humansize(nbytes):
     suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
@@ -98,22 +111,28 @@ def humansize(nbytes):
     f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
     return '%s %s' % (f, suffixes[i])
 
+
 def output_games(games):
     df = pd.DataFrame(games)
     df.sort_values("timePerByte", ascending=False, inplace=True)
     df["cumulativeTime"] = df["playtime"].cumsum()
     df["cumulativeSize"] = df["size"].cumsum()
-    df.cumulativeTime = df.cumulativeTime.apply(lambda x: '{:02d}:{:02d}'.format(*divmod(x, 60)))
+    df.cumulativeTime = df.cumulativeTime.apply(
+        lambda x: '{:02d}:{:02d}'.format(*divmod(x, 60)))
     df.cumulativeSize = df.cumulativeSize.apply(humansize)
 
-    print(f"\n{Fore.CYAN}Found and matched {df.shape[0]} installed games: {Style.RESET_ALL}")
-    print(df[["name", "sizeGB", "playtimeH", "hoursPerGB", "cumulativeSize", "cumulativeTime"]].to_string(index=False))
+    print(
+        f"\n{Fore.CYAN}Found and matched {df.shape[0]} installed games: {Style.RESET_ALL}")
+    print(df[["name", "sizeGB", "playtimeH", "hoursPerGB",
+          "cumulativeSize", "cumulativeTime"]].to_string(index=False))
+
 
 def output_unmatched(unmatched_games):
     df = pd.DataFrame(unmatched_games)
     df.sort_values("playtime", ascending=False, inplace=True)
 
-    print(f"\nFound {df.shape[0]} games not matched (eg. download not complete): ")
+    print(
+        f"\nFound {df.shape[0]} games not matched (eg. download not complete): ")
     print(df[["name", "playtimeH"]].to_string())
 
 
@@ -125,9 +144,6 @@ def main():
         output_unmatched(unmatched_games)
     os.system("pause")
 
+
 if __name__ == '__main__':
     sys.exit(main())
-
-
-
-
