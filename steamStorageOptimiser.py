28ae90cf560f0f4820df82719c790e396a85c275
Jake Martin <jm917@ic.ac.uk>
2022-06-05 20:01:50 +0100

    Switched to colorama for formatting

--- steamStorageOptimiser.py
+++ steamStorageOptimiser.py
@@ -6,33 +6,23 @@ import json
 import pandas as pd
 from hurry.filesize import size as prettySize
 from datetime import timedelta
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    OKGREY = '\033[30m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
+from colorama import init, Fore, Back, Style
+init()
 
 
 def load_config():
     try:
         with open('config.json') as f:
             config = json.load(f)
+        print(f"{Fore.GREEN}Loaded config file.{Style.RESET_ALL} To change, delete the config file to run setup again, or edit it directly.")
     except:
-        print(f"{bcolors.WARNING}No config file found.{bcolors.ENDC}")
+        print(f"{Fore.YELLOW}No config file found.{Style.RESET_ALL}")
         key = input("Enter your API key (create one here: https://steamcommunity.com/dev/apikey (domain is irrelevant)): ")
         steamid = input("Enter your 64-bit SteamID (eg. use this tool https://www.steamidfinder.com/): ")
         config = {"key" : key, "steamid" : steamid, "install_dir" : "C:\Program Files (x86)\Steam\steamapps"}
         with open("config.json", 'w') as f:
             json.dump(config, f)
-        input(f"{bcolors.OKGREEN}Saved new config file.{bcolors.ENDC} Press any key to continue.")
+        input(f"{Fore.GREEN}Saved new config file.{Style.RESET_ALL} Press any key to continue.")
     return config
 
 def get_api_response(config):
@@ -43,7 +33,7 @@ def get_api_response(config):
     try:
         api_response = api_response.json()["response"]["games"]
     except:
-        print(f"{bcolors.FAIL}API response invalid.{bcolors.ENDC} Expected data, recieved:\n{api_response.text}. \n{bcolors.WARNING}Check your config?{bcolors.ENDC}")
+        print(f"{Fore.RED}API response invalid.{Style.RESET_ALL} Expected data, recieved:\n{api_response.text}. \n{Fore.YELLOW}Check your config?{Style.RESET_ALL}")
         os.system("pause")
         sys.exit(1)
     return {game["appid"] : game for game in api_response}
@@ -53,7 +43,7 @@ def get_library_paths(config):
         with open(f"{config['install_dir']}\\libraryfolders.vdf") as f:
             libraries = vdf.parse(f)["libraryfolders"]
     except:
-        print(f"{bcolors.FAIL}Problem with libraryfolders file.{bcolors.ENDC} If Steam not installed at {bcolors.WARNING}{config['install_dir']}{bcolors.ENDC}, edit your config file and try again")
+        print(f"{Fore.RED}Problem with libraryfolders file.{Style.RESET_ALL} If Steam not installed at {Fore.YELLOW}{config['install_dir']}{Style.RESET_ALL}, edit your config file and try again")
         os.system("pause")
         sys.exit(1)
 
@@ -68,18 +58,17 @@ def match_games(config, api_dict):
 
     for library_path in get_library_paths(config):
         manifests = [ f.path for f in os.scandir(library_path) if not f.is_dir() and f.name[0] == 'a' ]
-        print(f"{bcolors.OKCYAN}Found {len(manifests)} in library {library_path}{bcolors.ENDC}")
+        print(f"{Fore.GREEN}Found {len(manifests)} in library {library_path}{Style.RESET_ALL}")
         for manifest in manifests:
             try:
                 with open(manifest) as f:
                     manifest = vdf.parse(f)
                 for game in manifest.values():
-                    #print(game["appid"])
                     appid = int(game["appid"])
                     try:
                         api_info = api_dict[appid]
                     except KeyError as e:
-                        print(f"{bcolors.OKGREY}Failed to find game {appid}: {game['name']}.{bcolors.ENDC}")
+                        print(f"{Style.DIM}Failed to match {appid}: {game['name']}.{Style.RESET_ALL}")
                     else:
                         playtime = api_info["playtime_forever"]
                         size = int(game["SizeOnDisk"])
@@ -117,7 +106,7 @@ def output_games(games):
     df.cumulativeTime = df.cumulativeTime.apply(lambda x: '{:02d}:{:02d}'.format(*divmod(x, 60)))
     df.cumulativeSize = df.cumulativeSize.apply(humansize)
 
-    print(f"\n{bcolors.HEADER}{bcolors.BOLD}{bcolors.UNDERLINE}Found and matched {df.shape[0]} installed games: {bcolors.ENDC}")
+    print(f"\n{Fore.CYAN}Found and matched {df.shape[0]} installed games: {Style.RESET_ALL}")
     print(df[["name", "sizeGB", "playtimeH", "hoursPerGB", "cumulativeSize", "cumulativeTime"]].to_string(index=False))
 
 def output_unmatched(unmatched_games):
