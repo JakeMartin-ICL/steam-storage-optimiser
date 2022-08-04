b9ca8221bd631b04c227d778c6ac7b1ae4002f3f
Jake Martin <jm917@ic.ac.uk>
2022-08-04 15:51:11 +0100

    Added database.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Steam Storage Optimiser
-A tool to help users decide which games to uninstall to save space.
+A tool to help users decide which games to install or uninstall to save space.
 
 ![image](https://user-images.githubusercontent.com/15602977/172073497-910b6b42-97b2-4735-9424-dd807d182f9d.png)
 
@@ -7,7 +7,7 @@ A tool to help users decide which games to uninstall to save space.
 When run, the tool will find the size-on-disk of all ***installed*** games, then use the Steam Web API to match each game with your 'hours played' to calculate an 'hours played per GB'. As a measure of how efficiently a game has captured your interest in the past for its size, it's likely a reasonably good predictor for the future too. Using the cumulative columns, it's easy to find a selection of games for a given amount of space that might keep you most entertained, plus show how much time that selection has occupied so far.
 
 ## Limitations
-The Steam Web API provides no method for finding the size-on-disk for a game. As such, the only reliable way of collecting this data is looking at the *actual* size of your current installations. This means that ***games that you own but don't currently have installed can't be shown in the list***. 
+The Steam Web API provides no method for finding the size-on-disk for a game. As such, the only reliable way of collecting this data is looking at the *actual* size of your current installations. ~~This means that games that you own but don't currently have installed can't be shown in the list.~~ **[Update 04/08/2022]** The optimiser now maintains a shared database to crowdsource this data, so uninstalled games can now be displayed too, as long as they've been seen installed before by anyone using this tool.
 
 ## Installation
 Download the [latest release](https://github.com/JakeMartin-ICL/steam-storage-optimiser/releases/latest) and run - no installation necessary.
--- src/steamStorageOptimiser.py
+++ src/steamStorageOptimiser.py
@@ -10,6 +10,8 @@ from cfs import error, warn, ok, note
 from colorama import init, Fore, Style
 init()
 
+update_api_threshold = 1024 ** 3
+
 
 def load_config():
     expected_keys = ["key", "steamid", "install_dir"]
@@ -48,7 +50,39 @@ def get_api_response(config):
     except json.decoder.JSONDecodeError:
         error(
             f"API response invalid. Expected data, recieved:\n{api_response.text}. \n{Fore.YELLOW}Check your config?{Style.RESET_ALL}{config}")
-    return {game["appid"]: game for game in api_response}
+    return api_response
+
+
+def get_size_from_api(appid):
+    url = f"https://eu5di55p9a.execute-api.eu-west-2.amazonaws.com/default/app/{appid}"
+    api_response = requests.get(url)
+    if api_response.status_code == requests.codes['not_found']:
+        return None
+    try:
+        api_response = api_response.json()
+    except json.decoder.JSONDecodeError:
+        error(
+            f"API response invalid. Expected data, recieved:\n{api_response.text}. Report this issue on GitHub.")
+    return api_response
+
+
+def update_api_size(appid, size):
+    url = f"https://eu5di55p9a.execute-api.eu-west-2.amazonaws.com/default/app/{appid}"
+    payload = {'size': size}
+
+    api_response = requests.put(url, params=payload)
+    if api_response.status_code != requests.codes['ok']:
+        error(f"Error while updating size database. Response: {api_response}")
+
+
+def add_to_db(appid, size):
+    url = f"https://eu5di55p9a.execute-api.eu-west-2.amazonaws.com/default/app/{appid}"
+    payload = {'size': size}
+
+    api_response = requests.post(url, params=payload)
+    if api_response.status_code != requests.codes['ok']:
+        error(
+            f"Error while adding new game to database. Response: {api_response}")
 
 
 def get_library_paths(config):
@@ -65,41 +99,60 @@ def get_library_paths(config):
     return library_paths
 
 
-def match_games(config, api_dict):
-    games = []
-    unmatched_games = []
+def get_installed_games(config):
+    installed_games = []
 
     for library_path in get_library_paths(config):
-        manifests = [f.path for f in os.scandir(
-            library_path) if not f.is_dir() and f.name[0] == 'a']
+        try:
+            manifests = [f.path for f in os.scandir(
+                library_path) if not f.is_dir() and f.name[0] == 'a']
+        except OSError:
+            warn(f"Library {library_path} missing - skipping.")
+            continue
         ok(f"Found {len(manifests)} in library {library_path}.")
         for manifest in manifests:
             try:
                 with open(manifest) as f:
                     manifest = vdf.parse(f)
-                for game in manifest.values():
-                    appid = int(game["appid"])
-                    try:
-                        api_info = api_dict[appid]
-                    except KeyError:
-                        note(f"Failed to match {appid}: {game['name']}.")
-                    else:
-                        playtime = api_info["playtime_forever"]
-                        size = int(game["SizeOnDisk"])
-                        if size != 0:
-                            games.append({"name": game["name"],
-                                          "bsize": size,
-                                          "size": prettySize(size),
-                                          "playtime": playtime,
-                                          "playtimeH": '{:02d}:{:02d}'.format(*divmod(playtime, 60)),
-                                          "timePerByte": playtime/size,
-                                          "hoursPerGB": (playtime/60)/(size/1073741824)})
-                        else:
-                            unmatched_games.append({"name": game["name"],
-                                                    "playtime": playtime,
-                                                    "playtimeH": '{:02d}:{:02d}'.format(*divmod(playtime, 60))})
+                installed_games += manifest.values()
             except SyntaxError:
                 pass
+
+    return {int(game['appid']): game for game in installed_games}
+
+
+def match_games(owned_games, installed_games):
+    games = []
+    unmatched_games = []
+
+    for game in owned_games:
+        appid = game['appid']
+        if appid in installed_games:
+            size = int(installed_games[appid]['SizeOnDisk'])
+            api_size = get_size_from_api(appid)
+            if api_size == None:
+                ok(f"Adding to database. This is the first time {game['name']} has been seen.")
+                add_to_db(appid, size)
+            elif abs(api_size - size) > update_api_threshold:
+                ok(f"Updating database. Your install size for {game['name']} is {humansize(size)}. The average size is {humansize(api_size)}")
+                update_api_size(appid, size)
+        else:
+            size = get_size_from_api(appid)
+
+        playtime = game["playtime_forever"]
+        if size != 0 and size != None:
+            games.append({"name": game["name"],
+                          "bsize": size,
+                          "size": prettySize(size),
+                          "playtime": playtime,
+                          "playtimeH": '{:02d}:{:02d}'.format(*divmod(playtime, 60)),
+                          "timePerByte": playtime/size,
+                          "hoursPerGB": (playtime/60)/(size/1073741824)})
+        else:
+            unmatched_games.append({"name": game["name"],
+                                    "playtime": playtime,
+                                    "playtimeH": '{:02d}:{:02d}'.format(*divmod(playtime, 60))})
+
     return (games, unmatched_games)
 
 
@@ -134,13 +187,15 @@ def output_unmatched(unmatched_games):
     df.sort_values("playtime", ascending=False, inplace=True)
 
     print(
-        f"\nFound {df.shape[0]} games not matched (eg. download not complete): ")
+        f"\nFound {df.shape[0]} games not matched. These games are not installed and not yet in the database: ")
     print(df[["name", "playtimeH"]].to_string())
 
 
 def main():
     config = load_config()
-    games, unmatched_games = match_games(config, get_api_response(config))
+    owned_games = get_api_response(config)
+    installed_games = get_installed_games(config)
+    games, unmatched_games = match_games(owned_games, installed_games)
     output_games(games)
     if len(unmatched_games) > 0:
         output_unmatched(unmatched_games)
