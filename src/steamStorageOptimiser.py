1d165fe7c9da4069bda8445827a3700853f276e7
Jake Martin <jake_martin_@outlook.com>
2022-12-18 01:30:05 +0000

    Fixed Windows 10 detection Python's platform library is currently broken and cannot detect Windows 11

--- src/steamStorageOptimiser.py
+++ src/steamStorageOptimiser.py
@@ -1,6 +1,5 @@
 import json
 import os
-import platform
 import sys
 from pathlib import Path
 
@@ -16,7 +15,7 @@ init()
 
 update_api_threshold = 1024 ** 3
 tick = '√' if getattr(
-    sys, 'frozen', False) and platform.release() == '10' else '✓'
+    sys, 'frozen', False) and sys.platform == 'win32' and sys.getwindowsversion().build < 22000 else '✓'
 
 
 def load_config():
